08a9c74dba9ec1e2d0629b2143fb8e402f5f4961
leee1846 <leee1846@gmail.com>
2021-06-01 23:03:01 +0900

    test:  http request 의 이해

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-const _ = require("lodash");
+const http = require("http");
 
-const items = [1, [2, [3, [4, [5]]]]];
-const nreItems = _.flattenDeep(items);
-console.log(nreItems);
-console.log("나나");
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>home page</h1>");
+    res.end();
+  } else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>about page</h1>");
+    res.end();
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>page not found</h1>");
+    res.end();
+  }
+});
+
+server.listen(5000);
