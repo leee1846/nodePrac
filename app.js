e012eab56b2ec7a224cd1e08cf5c4479246a79a9
leee1846 <leee1846@gmail.com>
2021-06-01 23:33:37 +0900

    test: nav바 서버에서 브라우저로 출력하기

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const http = require("http");
+const { readFileSync } = require("fs");
+
+const homePage = readFileSync("./nav/index.html");
+const style = readFileSync("./nav/styles.css");
+const logo = readFileSync("./nav/logo.svg");
+const script = readFileSync("./nav/browser-app.js");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
   if (url === "/") {
     res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>home page</h1>");
+    res.write(homePage);
     res.end();
-  } else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>about page</h1>");
+  } else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(style);
+    res.end();
+  } else if (url === "/logo.svg") {
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.write(logo);
+    res.end();
+  } else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.write(script);
     res.end();
   } else {
     res.writeHead(404, { "content-type": "text/html" });
