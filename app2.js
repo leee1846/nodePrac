ebb33142b5b8a2d0214ec93b51bb4faca549714e
leee1846 <leee1846@gmail.com>
2021-06-03 14:58:20 +0900

    test: restAPI get으로 params와 query사용해보기

--- app2.js
+++ app2.js
@@ -21,7 +21,32 @@ app.get(`/api/products/:productId`, (req, res) => {
   const singleProduct = products.find(
     (product) => product.id === Number(productId)
   );
+  if (!singleProduct) {
+    return res.status(404).send("product does not exist");
+  }
   res.json(singleProduct);
+});
+
+app.get("api/products/:productId/reviews/:reviewId", (req, res) => {
+  const { productId, reviewId } = req.params;
+});
+
+app.get(`/api/v1/query`, (req, res) => {
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+  if (sortedProducts.length < 1) {
+    return res.status(200).json({ sucess: true, data: [] });
+  }
+  res.status(200).json(sortedProducts);
 });
 
 app.listen(5000, () => {
