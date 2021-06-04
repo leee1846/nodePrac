9870dbcb17cbf549140f871ee189941782357324
leee1846 <leee1846@gmail.com>
2021-06-04 15:27:53 +0900

    controller로 로직함수 나누기

--- routes/people.js
+++ routes/people.js
@@ -1,52 +1,16 @@
+const {
+  getPeople,
+  createPerson,
+  updatePerson,
+  deletePerson,
+} = require("../controller/people");
 const express = require("express");
+
 const router = express.Router();
 
-let { people } = require("../data");
-
-router.get("/", (req, res) => {
-  res.status(200).json({ success: true, data: people });
-});
-
-router.post("/", (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, message: "please provide name value" });
-  }
-  res.status(201).json({ success: true, person: name });
-});
-
-router.put("/:id", () => {
-  const { id } = req.params;
-  const { name } = req.body;
-  const person = people.find((person) => person.id === Number(id));
-
-  if (!person) {
-    return res
-      .status(400)
-      .json({ success: false, message: `no person with id ${id}` });
-  }
-  const newPeople = people.map((person) => {
-    if (person.id === Number(id)) {
-      person.name = name;
-    }
-    return person;
-  });
-  res.status(200).json({ success: true, data: newPeople });
-});
-
-router.delete(`/:id`, (req, res) => {
-  const { id } = req.params;
-  const person = people.find((person) => person.id === Number(id));
-
-  if (!person) {
-    return res
-      .status(400)
-      .json({ success: false, message: `no person with id ${id}` });
-  }
-  const newPeople = people.filter((person) => person.id !== Number(id));
-  res.status(200).json({ sccess: true, data: newPeople });
-});
+router.get("/", getPeople);
+router.post("/", createPerson);
+router.put("/:id", updatePerson);
+router.delete(`/:id`, deletePerson);
 
 module.exports = router;
